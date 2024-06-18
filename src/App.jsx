a52789ea5ccb0580d49c7814d030929d48724875
dipu-37 <razibul1105nstu@gmail.com>
2024-06-18 17:38:08 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import SimpleForm from './components/SimpleForm'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <SimpleForm></SimpleForm>
+      
+    </>
+  )
+}
+
+export default App
