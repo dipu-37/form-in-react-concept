556ba40a427e0f6e4185569abff675f375476360
dipu-37 <razibul1105nstu@gmail.com>
2024-06-20 21:14:05 +0700

    46-5 (Advanced) Reusable Form Using Props And Children

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@
 import './App.css'
 import HookForm from './components/HookForm'
 import RefForm from './components/RefForm'
+import ReuseAbleForm from './components/ReuseAbleForm'
 import SimpleForm from './components/SimpleForm'
 import StatefullForm from './components/StatefullForm'
 
 function App() {
- 
-
+   const handleSignUpSubmit = data=>{
+        // e.preventDefault();
+        console.log('sign up form', data)
+    }
+   const handleUpdatProfile= data=>{
+    // e.preventDefault()
+    console.log('updat form', data)
+   }
   return (
     <>
       
@@ -15,7 +22,15 @@ function App() {
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefullForm></StatefullForm> */}
       {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      <ReuseAbleForm formTitle={'sign up'} handlSubmit={handleSignUpSubmit}>
+        <h2>pleace sign up </h2>
+        <p>must sign up</p>
+      </ReuseAbleForm>
+      <ReuseAbleForm formTitle={'Profile update'} submitbtntext='update' handlSubmit={handleUpdatProfile}>
+      <h2>pleace update </h2>
+      <p>must update</p>
+      </ReuseAbleForm>
     </>
   )
 }
---
+++ src/components/ReuseAbleForm.jsx
@@ -0,0 +1,31 @@
+
+const ReuseAbleForm = ({formTitle,handlSubmit, submitbtntext='submit' , children}) => {
+    // const handlSubmit= e=>{
+    //     e.preventDefault();
+    // }
+    const handleLocalSubmit =e =>{
+        e.preventDefault();
+        const data ={
+            name: e.target.name.value,
+            email: e.target.email.value,
+            password: e.target.password.value
+        }
+        handlSubmit(data)
+    }
+    return (
+        <div>
+           {children}
+             <form onSubmit={handleLocalSubmit}>
+                <input type="text" name="name" />
+                <br />
+                <input type="email" name="email" id="" />
+                <br />
+                <input type="password" name="password" />
+                <br />
+                <input type="submit" value={submitbtntext} />
+            </form>
+        </div>
+    );
+};
+
+export default ReuseAbleForm;
